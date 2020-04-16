f62d35442f064631934f3285e61a3eba2527a799
Michael <mobrien@colgate.edu>
2020-04-16 11:55:36 -0400

    basic chat functionality working, can communicate between two users but still rough - can't tell who is saying what

--- src/App.js
+++ src/App.js
@@ -5,10 +5,26 @@ import { useImmer } from "use-immer";
 
 import "./App.css";
 
+const Online = (props) =>
+  props.data.map((message) => <li id={message[0]}>{message[1]}</li>);
+
+const Messages = (props) =>
+  props.data.map((message) =>
+    message[0] !== "" ? (
+      <li>
+        <strong>{message[0]}</strong> :{" "}
+        <div className="innermsg">{message[1]}</div>
+      </li>
+    ) : (
+      <li className="update">{message[1]}</li>
+    )
+  );
+
 function App() {
   const [id, setId] = useState("");
   const [nameInput, setNameInput] = useState("");
   const [room, setRoom] = useState("");
+  const [input, setInput] = useState("");
   const [socket] = useSocket("https://open-chat-naostsaecf.now.sh");
   const [messages, setMessages] = useImmer([]);
   const [online, setOnline] = useImmer([]);
@@ -25,29 +41,84 @@ function App() {
     socket.emit("join", id, room);
   };
 
+  const handleSend = (event) => {
+    event.preventDefault();
+    if (input !== "") {
+      socket.emit("chat message", input, room);
+      setInput("");
+    }
+  };
+
   useEffect(() => {
-    socket.on("update", (user, message) =>
+    socket.on("update", (message) =>
+      setMessages((draft) => {
+        draft.push(["", message]);
+      })
+    );
+    socket.on("message que", (user, message) =>
       setMessages((draft) => {
         draft.push([user, message]);
       })
     );
-  });
+    socket.on("people-list", (people) => {
+      let newState = [];
+      for (let person in people) {
+        newState.push([people[person].id, people[person].user]);
+      }
+      setOnline((draft) => {
+        draft.push(...newState);
+      });
+      console.log(online);
+    });
+    socket.on("add-person", (user, id) => {
+      setOnline((draft) => {
+        draft.push([id, user]);
+      });
+    });
 
-  return id !== "" ? (
-    <div>Hello</div>
+    socket.on("remove-person", (id) => {
+      setOnline((draft) => draft.filter((user) => user[0] !== id));
+    });
+
+    socket.on("chat message", (user, message) => {
+      setMessages((draft) => {
+        draft.push([user, message]);
+      });
+    });
+  }, []);
+
+  return id ? (
+    <section style={{ display: "flex", flexDirection: "row" }}>
+      <ul id="messages">
+        <Messages data={messages} />
+      </ul>
+      <ul id="online">
+        {" "}
+        &#x1f310; : <Online data={online} />{" "}
+      </ul>
+      <div id="sendform">
+        <form onSubmit={(e) => handleSend(e)} style={{ display: "flex" }}>
+          <input id="m" onChange={(e) => setInput(e.target.value.trim())} />
+          <button style={{ width: "75px" }} type="submit">
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
   ) : (
-    <div className="chatbox">
+    <div style={{ textAlign: "center", margin: "30vh auto", width: "70%" }}>
       <form onSubmit={(event) => handleSubmit(event)}>
         <input
           id="name"
-          onChange={(event) => setNameInput(event.target.value.trim())}
+          onChange={(e) => setNameInput(e.target.value.trim())}
           required
-          placeholder="Name"
+          placeholder="What is your name .."
         />
+        <br />
         <input
           id="room"
-          onChange={(event) => setRoom(event.target.value.trim())}
-          placeholder="room"
+          onChange={(e) => setRoom(e.target.value.trim())}
+          placeholder="What is your room .."
         />
         <br />
         <button type="submit">Submit</button>
